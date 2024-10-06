6641926fbabe8cfb6bf4f8fe0b3ef585a03e79b0
Nelli Gorlova <nelli7035@gmail.com>
2024-10-07 04:52:36 +0600

    Created the Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Header = () => {
+  return (
+    <div className="bg-info">
+      <div className="container">
+        <h1 className="p-2 text-center">Welcome!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
